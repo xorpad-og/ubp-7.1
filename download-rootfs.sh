a6f26334547fc027545f361935fbc331312a6ad9
xorpad <xorpad@nixphone.info>
2017-12-02 03:37:37 -0500

    fixing up build tree

--- download-rootfs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Downloading rootfs to current directory."
-wget http://zerorax.guru/rootfs-xenial-aarch64.tgz
-echo "Download complete, rootfs-xenial-aarch64.tgz is now in your current directory."
-
